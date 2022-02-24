cdbb311a4579c2767b81a1c6a2ccc0f555caca49
Raymond Chen <ahrchen@gmail.com>
2022-02-23 20:01:04 -0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Raymond Chen on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
