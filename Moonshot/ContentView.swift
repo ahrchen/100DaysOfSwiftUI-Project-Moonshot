73553dd200b37e3a5bb81a0f676c342893a33e8d
Raymond Chen <ahrchen@gmail.com>
2022-02-26 12:36:13 -0800

    Add option for list view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,6 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     
+    struct ContentScrollView: View {
+        let astronauts: [String: Astronaut]
+        let missions: [Mission]
+        let columns: [GridItem]
+        
+        var body: some View {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label : {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height:  100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                                
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                            
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+        }
+    }
+    
     struct GridLayout: View {
         let astronauts: [String: Astronaut]
         let missions: [Mission]
@@ -17,57 +63,44 @@ struct ContentView: View {
         ]
         
         var body: some View {
-            LazyVGrid(columns: columns) {
-                ForEach(missions) { mission in
-                    NavigationLink {
-                        MissionView(mission: mission, astronauts: astronauts)
-                    } label : {
-                        VStack {
-                            Image(mission.image)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 100, height:  100)
-                                .padding()
-                            
-                            VStack {
-                                Text(mission.displayName)
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                                Text(mission.formattedLaunchDate)
-                                    .font(.caption)
-                                    .foregroundColor(.white.opacity(0.5))
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .background(.lightBackground)
-                            
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.lightBackground)
-                        )
-                        
-                    }
-                }
-            }
-            .padding([.horizontal, .bottom])
+            ContentScrollView(astronauts: astronauts, missions: missions, columns: columns)
         }
     }
     
+    struct ListLayout: View {
+        let astronauts: [String: Astronaut]
+        let missions: [Mission]
+        let columns =  [
+            GridItem(.adaptive(minimum: .infinity))
+        ]
+        
+        var body: some View {
+            ContentScrollView(astronauts: astronauts, missions: missions, columns: columns)
+        }
+    }
+    
+    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
+    @State private var showingGrid = true
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                GridLayout(astronauts: astronauts, missions: missions)
-                
+            Group {
+                if showingGrid {
+                    GridLayout(astronauts: astronauts, missions: missions)
+                } else {
+                    ListLayout(astronauts: astronauts, missions: missions)
+                }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Toggle(showingGrid ? "Grid View" : "List View", isOn: $showingGrid)
+                }
+            }
         }
     }
 }
